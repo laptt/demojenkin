2ef995913c63f2ac8c44ac25dc07ec5386b185c9
laptt <laptt@quanhta.vn>
2023-03-12 08:59:39 -0700

    asd

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Pageasd'),
+      home: const MyHomePage(title: 'Flutter Demo Homeaaa Pageasddd'),
     );
   }
 }
